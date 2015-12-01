f2884026d3dfb3906736fe25db4623974a82b426
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-12-01 10:31:25 -0500

    Add Edit Method into Compliments Controller

--- controllers/complimentsController.js
+++ controllers/complimentsController.js
@@ -19,7 +19,13 @@ var complimentsController = {
     // redirect to compliment
     res.redirect("/compliments")
   }
-  // edit action code goes here...
+
+  edit: function(req, res){
+    res.render('/compliments/edit.hbs', {
+      id: req.params.id,
+      compliment: Compliment.find([req.params.id])
+    });
+  },
 
  // update action code goes here...
 }
